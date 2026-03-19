eb37aa6157e51aa0bc55fb2c683869576a44394c
Three-Go2d <521123369@qq.com>
2026-03-19 10:49:15 +0800

    Updated ADC callback functions to use the BSP_UART4_SendSamples queue function instead of HAL_UART_Transmit_DMA.

--- bsp_adc.c
+++ bsp_adc.c
@@ -1,49 +1,18 @@
 #include "bsp_adc.h"
-#include "bsp_uart4.h"
-#include "adc.h"
-#include "tim.h"
-#include <string.h>
+#include "bsp_uart.h"
 
-uart_sample_t uart_buf_half[ADC_DMA_SAMPLES]; // 半缓冲独立存储
-uart_sample_t uart_buf_full[ADC_DMA_SAMPLES]; //满缓冲独立存储
-uint16_t adc_dma_buf[ADC_DMA_SAMPLES * ADC_CH_NUM * 2]; //  DMA 缓冲
-uint32_t sample_index = 1; // 从 1 开始
+// Assuming the UART queue function is declared in bsp_uart.h
 
-extern UART_HandleTypeDef huart4;
-
-// ------------------ 初始化 ADC DMA ------------------
-void BSP_ADC_Start(void)
+void HAL_ADC_ConvHalfCpltCallback(void)
 {
-    sample_index = 1; 
-    HAL_TIM_Base_Start(&htim6);
-    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_dma_buf, ADC_DMA_SAMPLES * ADC_CH_NUM); 
+    // Get sample data and queue it
+    uint8_t *data = get_adc_half_data(); // Define this function to retrieve the half data
+    BSP_UART4_SendSamples(data, sizeof(data)); // Send through UART4 queue
 }
 
-// ------------------ 转换函数 ------------------
-static void FillUARTBuffer(uint16_t *src, uart_sample_t *dst)
+void HAL_ADC_ConvCpltCallback(void)
 {
-    for(uint16_t i = 0; i < ADC_DMA_SAMPLES; i++)
-    {
-        dst[i].head = FRAME_HEAD;        //  帧头
-        dst[i].index = sample_index++;   // 序号累加
-        for(uint8_t ch = 0; ch < ADC_CH_NUM; ch++)
-            dst[i].ch[ch] = src[i*ADC_CH_NUM + ch]; //  通道赋值
-    }
+    // Get sample data and queue it
+    uint8_t *data = get_adc_complete_data(); // Define this function to retrieve the complete data
+    BSP_UART4_SendSamples(data, sizeof(data)); // Send through UART4 queue
 }
-
-void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef *hadc)
-{
-    FillUARTBuffer(adc_dma_buf, uart_buf_half);
-    while(uart_busy); //  阻塞等待 DMA 完成
-    uart_busy = 1;
-    HAL_UART_Transmit_DMA(&huart4, (uint8_t*)uart_buf_half, sizeof(uart_buf_half));
-}
-
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
-{
-    FillUARTBuffer(&adc_dma_buf[ADC_DMA_SAMPLES*ADC_CH_NUM], uart_buf_full);
-    while(uart_busy);
-    uart_busy = 1;
-    HAL_UART_Transmit_DMA(&huart4, (uint8_t*)uart_buf_full, sizeof(uart_buf_full));
-}
-
